4b627c7860860479cef46b66ef133e5787afe5cf
y-yamanaka0509 <y_yamanaka@wisenet.co.jp>
2016-11-12 18:46:06 +0900

    Add 課題5_1
    課題5_1作成

---
+++ Kadai5_const.java
@@ -0,0 +1,17 @@
+
+public class Kadai5_const {
+
+	/**
+	 * 接続文字列C
+	 *
+	 */
+	/** My SQLのurl */
+	public static final String SQL_URL = "jdbc:mysql://localhost/test";
+
+	/** My SQLのuser */
+	public static final String SQL_USER = "root";
+
+	/** My SQLのpassword */
+	public static final String SQL_PASS = "";
+
+}
